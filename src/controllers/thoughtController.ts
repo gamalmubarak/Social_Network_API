f6f4eb7fcfa9931fb6d97ee8d9ed61b437a0e514
gamalmubarak <eng-gemy@hotmail.com>
2025-04-24 00:25:19 -0400

    getThoughts and getThoughtById added

--- src/controllers/thoughtController.ts
+++ src/controllers/thoughtController.ts
@@ -26,3 +26,23 @@ export const createThought = async (req: Request<{}, {}, ThoughtBody>, res: Resp
     res.status(500).json({ message: 'Failed to create thought', error: err });
   }
 };
+export const getThoughts = async (_req: Request, res: Response) => {
+    try {
+      const thoughts = await Thought.find();
+      res.status(200).json(thoughts);
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to retrieve thoughts', error: err });
+    }
+  };
+  
+  export const getThoughtById = async (req: Request<ThoughtParams>, res: Response) => {
+    try {
+      const thought = await Thought.findById(req.params.id);
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      return res.status(200).json(thought);
+    } catch (err) {
+      return res.status(500).json({ message: 'Failed to retrieve thought', error: err });
+    }
+  };
